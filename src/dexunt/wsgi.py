2bb368b4a7688294d661b0beadaca047f14e2841
Zoeza <zahrahabitaa@gmail.com>
2022-11-29 23:30:45 +0100

    Update wsgi.py

--- src/dexunt/wsgi.py
+++ src/dexunt/wsgi.py
@@ -11,7 +11,7 @@ import os
 from dotenv import load_dotenv, find_dotenv
 
 from django.core.wsgi import get_wsgi_application
-load_dotenv()
+load_dotenv(find_dotenv())
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dexunt.settings')
 
 application = get_wsgi_application()

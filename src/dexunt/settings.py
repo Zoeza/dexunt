85ae8ce70f0c47fc726509ccc0ae12d1656af765
Zoeza <zahrahabitaa@gmail.com>
2022-11-29 19:31:42 +0100

    Update settings.py

--- src/dexunt/settings.py
+++ src/dexunt/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-
 import os.path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =  os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-8w)708#%e+6-xla@tay-v6*#s$yws1q!_td+x_!drm4lvdu)od'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
